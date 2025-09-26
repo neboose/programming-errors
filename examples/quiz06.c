f8c87944a08fea3a5831a3af1af43e6d71c489ba
neboose <ben.klump@gmail.com>
2025-09-26 13:07:29 +0000

    feat: suggest possible solutions

---
+++ examples/quiz06.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float get_float(char *prompt);
+
+int main(void)
+{
+    float answer = get_float("Q1: 5 / 2  ");
+    int a = 5;
+    if (answer == (float)a / 2)
+    {
+        printf("Correct!\n");
+    }
+    else
+    {
+        printf("Incorrect!\n");
+    }
+    return 0;
+}
+
+float get_float(char *prompt)
+{
+    printf("%s", prompt);
+    float input;
+    scanf("%f", &input);
+    return input;
+}
