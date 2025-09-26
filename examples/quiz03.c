8bc8d0f5e7dd2755f1b1a197894361418be714c3
neboose <ben.klump@gmail.com>
2025-09-26 12:47:50 +0000

    feat: reveal the truth to why the quiz is incorrect

---
+++ examples/quiz03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float get_float(char *prompt);
+
+int main(void)
+{
+    float answer = get_float("Q1: 5 / 2  ");
+    printf("%f\n", 5 / 2);
+    if (answer == 5 / 2)
+    {
+        printf("Correct!\n");
+    }
+    else
+    {
+        printf("Incorrect!\n");
+    }
+    return 0;
+}
+
+float get_float(char *prompt)
+{
+    printf("%s", prompt);
+    float input;
+    scanf("%f", &input);
+    return input;
+}
